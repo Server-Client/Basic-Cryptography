a93abf46cba9089f690a9f589391327fde82fc22
Server.Host <87484486+Server-Client@users.noreply.github.com>
2021-07-21 07:29:57 -0400

    Update RMC.py

--- RMC.py
+++ RMC.py
@@ -1,3 +1,21 @@
 x = input('Type String: ')
-while True:
-  y = input('
+y = input('Character to Replace & Character to Replace with (ex: e,r.r,t.h,x): ')
+z = y.split('.')
+w = str(z).replace("'", "").replace('[', '').replace(']', '').replace(',', '')
+u = w.replace(' ', '')
+v = len(u)/2
+if v == 2.0:
+    a,b,c,d = u
+    print(x.replace(a,b).replace(c,d))
+
+elif v == 3.0:
+    a,b,c,d,e,f = u
+    print(x.replace(a,b).replace(c,d).replace(e,f))
+
+elif v == 4.0:
+    a,b,c,d,e,f,g,h = u
+    print(x.replace(a,b).replace(c,d).replace(e,f).replace(g,h))
+
+elif v == 5.0:
+    a,b,c,d,e,f,g,h,i,j = u
+    print(x.replace(a,b).replace(c,d).replace(e,f).replace(g,h).replace(i,j))
